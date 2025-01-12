7cd9b9a78b22d3d2d857c03ded22dda56b0d07ba
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2025-01-12 17:54:19 +0000

    fixed naming of scenario template

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 PROMPT_TEMPLATE_PATH = './data/prompt_template.txt'
-TEST_PROMPT_PATH = './data/test_prompt.txt'
+PROMPT_TEMPLATE_SCENARIO_ONLY_PATH = './data/prompt_template_scenario_only.txt'
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
