f309dc539b8ac32313a99f041648589c2a3835f4
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2025-01-17 08:17:07 +0000

    both server and ipnyb work

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-PROMPT_TEMPLATE_PATH = './data/system_prompt_template.txt'
-TEACHER_TEMPLATE_PATH = './data/teacher_prompt_template.txt'
+PROMPT_TEMPLATE_PATH = './prompts/system_prompt_template.txt'
+TEACHER_TEMPLATE_PATH = './prompts/teacher_prompt_template.txt'
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
